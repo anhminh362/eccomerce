8e2bb46439d23ed5f0daf38248d19432601e4c56
Khanh <khanhabcdd1@gmail.com>
2023-06-10 16:20:18 +0700

    a

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,6 +1,59 @@
+import { useState } from "react";
+import axios from "axios";
+import './Header.css'
 function Header() {
+    const [showForm, setShowForm] = useState(false);
+    const [fullName, setFullName] = useState("");
+    const [content, setContent] = useState("");
+
+    const handleClick = () => {
+        setShowForm(true);
+      };
+    
+      const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(fullName, content);
+        
+        axios.post('http://localhost:3000/forms', {
+            fullName:fullName,
+            content:content
+          })
+          .then(function (response) {
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+
+
+
+        setFullName("");
+        setContent("");
+        // Đóng form
+        setShowForm(false);
+      };
+
     return(
         <>
+    {showForm &&(
+            <div  className="form-contact">
+            <form onSubmit={handleSubmit} className="form">
+                <h2>Contact</h2>
+                <input  className="form-control input"
+                type="text"
+                placeholder="Họ tên"
+                value={fullName}
+                onChange={(e) => setFullName(e.target.value)}
+                />
+                <textarea  className="form-control textarea"
+                placeholder="Nội dung"
+                value={content}
+                onChange={(e) => setContent(e.target.value)}
+                ></textarea>
+                <button type="submit" className="submit">Gửi</button>
+            </form>
+            </div>
+    )}
         {/* banner bg main start */}
         <div className="banner_bg_main">
           {/* header top section start */}
@@ -64,7 +117,7 @@ function Header() {
                   <a href="electronic.html">Electronic</a>
                   <a href="jewellery.html">Jewellery</a>
                 </div>
-                <span className="toggle_icon" onClick={"openNav()"}>
+                <span className="toggle_icon" onClick={handleClick}>
                   <img src="images/toggle-icon.png" />
                 </span>
                 <div className="dropdown">
